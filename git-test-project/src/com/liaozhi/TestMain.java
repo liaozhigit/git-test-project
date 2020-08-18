09c561074b981417a3a1260c36e5df6df7cb9c65
liaozhigit <18682146878@qq.com>
2020-08-19 02:56:52 +0800

    add method SysOut

--- git-test-project/src/com/liaozhi/TestMain.java
+++ git-test-project/src/com/liaozhi/TestMain.java
@@ -1,9 +1,14 @@
 package com.liaozhi;
 
+import sun.tools.jar.Main;
+
 /**
  * @Author liaozhi
  * @Date: 2020/8/19 2:42
  * @Version 1.0
  */
 public class TestMain {
+    public void SysOut(){
+        System.out.printf("hello wolrd");
+    }
 }
